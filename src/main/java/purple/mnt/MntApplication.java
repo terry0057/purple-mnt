dcf9de3e6a33049b14bfe5a0fd6522b8d382f3a4
terry0057 <jy471852098@sina.com>
2017-12-13 22:42:55 +0800

    Perfect the project

---
+++ src/main/java/purple/mnt/MntApplication.java
@@ -0,0 +1,12 @@
+package purple.mnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MntApplication.class, args);
+	}
+}
